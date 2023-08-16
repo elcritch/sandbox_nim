5f5d15b6a919468c941bd65ebbbc79918fa00281
Jaremy Creechley <creechley@gmail.com>
2023-08-16 12:51:51 -0700

    updates

--- example_seq_manual.nim
+++ example_seq_manual.nim
@@ -23,9 +23,8 @@ proc thread1(val: int) {.thread.} =
     myBytes.data = 22
     shared = myBytes
     echo "thread1: sent, left over: ", repr myBytes
-
     signal(event)
-
+    echo "thread1: wait"
     wait(event)
     echo "thread1: free: ", repr myBytes.data
 
@@ -33,25 +32,20 @@ proc thread2(val: int) {.thread.} =
   echo "thread2: wait"
   {.cast(gcsafe).}:
     wait(event)
-    
-    echo "thread2: receiving ", cast[pointer](shared).repr
+    echo "thread2: receiving: ", cast[pointer](shared).repr
     let msg = shared
+    echo "thread2: received: ", cast[pointer](msg).repr
     if msg != nil:
-      echo "thread2: received: ", cast[pointer](msg).repr
       echo "thread2: received: ", msg.data
       dealloc(msg)
-      echo "thread2: done: ", cast[pointer](msg).repr
+    echo "thread2: done: ", cast[pointer](msg).repr
     signal(event)
 
 proc main() =
   echo "running"
-
   event = initEvent()
   createThread(threads[0], thread1, 0)
   createThread(threads[1], thread2, 1)
-
   joinThreads(threads)
 
-
-when isMainModule:
-  main()
+main()
