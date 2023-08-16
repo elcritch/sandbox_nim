e5121acff3315a08bf49ef2c063309e98fa4636b
Jaremy Creechley <creechley@gmail.com>
2023-08-16 15:31:51 -0700

    add orc version to test

---
+++ threading_gc_refs/example_seq_gcref_unref_split_thread_orc.nim
@@ -0,0 +1,92 @@
+
+import std/os
+import std/locks
+import std/atomics
+
+import events
+
+var threads: array[2,Thread[int]]
+
+var
+  shareDataIsFreed: AtomicFreed
+
+type
+  Buffer = object
+    data: int
+
+proc `=destroy`*(x: var Buffer) =
+  echo "Buffer: destroy: ", cast[pointer](addr(x)).repr
+  discard shareDataIsFreed.incrFreedValue()
+
+var
+  shareData: ref Buffer
+  event: Event
+  eventAfterGcFree: Event
+
+proc thread1(val: int) {.thread.} =
+  echo "thread1: sending"
+  {.cast(gcsafe).}:
+    os.sleep(100)
+
+    var myBytes: ref Buffer
+    myBytes.new()
+    myBytes.data = 10
+
+    shareData = myBytes
+    GC_ref(shareData)
+    echo "thread1: sent, left over: ", repr myBytes
+    signal(event)
+
+    wait(event)
+    myBytes = nil
+    echo "thread1: post send: ", cast[pointer](myBytes).repr
+
+    GC_fullCollect()
+    signal(eventAfterGcFree)
+
+    wait(eventAfterGcFree)
+    echo "thread1: gc_unref and gc_collect"
+
+    GC_fullCollect()
+    os.sleep(100)
+    echo "thread1: finish"
+    assert getFreedValue(shareDataIsFreed) == 1, "shareData should be freed by now"
+    echo "thread1: done"
+
+proc thread2(val: int) {.thread.} =
+  echo "thread2: wait"
+  {.cast(gcsafe).}:
+    wait(event)
+    
+    echo "thread2: receiving ", cast[pointer](shareData).repr
+    var msg = shareData
+    echo "thread2: received: ", repr msg, "; shareData: ", cast[pointer](shareData).repr
+
+    echo "thread2: deref: "
+
+    signal(event)
+    wait(eventAfterGcFree)
+    echo "thread2: after gc_collect: ", repr msg
+    assert getFreedValue(shareDataIsFreed) == 0
+
+    GC_unref(shareData)
+    shareData = nil
+    msg = nil
+    signal(eventAfterGcFree)
+    echo "thread2: done"
+
+proc main() =
+  echo "running"
+
+  event = initEvent()
+  eventAfterGcFree = initEvent()
+  shareDataIsFreed = newFreedValue(0)
+  createThread(threads[0], thread1, 0)
+  createThread(threads[1], thread2, 1)
+
+  joinThreads(threads)
+  os.sleep(100)
+
+
+when isMainModule:
+  main()
