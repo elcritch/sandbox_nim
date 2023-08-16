7dcb3c9422cbf3b417b4e62938b39d1ec3778302
Jaremy Creechley <creechley@gmail.com>
2023-08-16 13:24:26 -0700

    cleanup locks

--- example_seq_sharedptr.nim
+++ example_seq_sharedptr.nim
@@ -10,34 +10,32 @@ var threads: array[2,Thread[int]]
 
 var
   # create a channel to send/recv strings
-  shareVal: SharedPtr[seq[char]]
+  shareVal: SharedPtr[int]
   event: Event
 
 proc thread1(val: int) {.thread.} =
   echo "thread1"
   {.cast(gcsafe).}:
     os.sleep(100)
-    withLock(event.lock):
-      # var myBytes = newSharedPtr(@"hello")
-      var myBytes = newSharedPtr(22)
-      echo "thread1: sending: ", myBytes
+    # var myBytes = newSharedPtr(@"hello")
+    var myBytes = newSharedPtr(22)
+    echo "thread1: sending: ", myBytes
 
-      shareVal = myBytes
-      echo "thread1: sent over: ", myBytes
-      echo "thread1: sent, left over: ", repr shareVal
-      signal(event.cond)
-      # os.sleep(500)
+    shareVal = myBytes
+    echo "thread1: sent over: ", myBytes
+    echo "thread1: sent, left over: ", repr shareVal
+    signal(event)
+    # os.sleep(500)
 
 proc thread2(val: int) {.thread.} =
   echo "thread2"
   {.cast(gcsafe).}:
-    withLock(event.lock):
-      wait(event.cond, event.lock)
-      echo "thread2: receiving"
-      var msg = shareVal
-      echo "thread2: received: ", repr msg[]
-      # echo "msg: ", msg[].len()
-      # os.sleep(100)
+    wait(event)
+    echo "thread2: receiving"
+    var msg = shareVal
+    echo "thread2: received: ", repr msg[]
+    # echo "msg: ", msg[].len()
+    # os.sleep(100)
 
 proc main() =
   echo "running"
