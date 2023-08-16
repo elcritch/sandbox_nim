dd30c42ad8587592472c3f8a31270e96c94a1138
Jaremy Creechley <creechley@gmail.com>
2023-08-16 13:11:42 -0700

    cleanup

--- example_seq_gcref_use_after_free.nim
+++ example_seq_gcref_use_after_free.nim
@@ -14,6 +14,7 @@ var
   # create a channel to send/recv strings
   shareSeq: ref Buffer
   event: Event
+  eventAfterGcFree: Event
 
 proc `=destroy`*(x: var Buffer) =
   echo "Buffer: destroy: ", cast[pointer](addr(x)).repr
@@ -23,22 +24,21 @@ proc thread1(val: int) {.thread.} =
   {.cast(gcsafe).}:
     os.sleep(100)
 
-    block:
-      var myBytes: ref Buffer
-      myBytes.new(proc (x: ref Buffer) = echo "thread1: freeing: ", cast[pointer](x).repr)
-      myBytes.data = 10
-      # GC_ref(myBytes)
-      # GC_unref(myBytes)
-      shareSeq = myBytes
-      echo "thread1: sent, left over: ", repr myBytes
-      signal(event)
-      wait(event)
-      # GC_unref(myBytes)
-      myBytes = nil
-      echo "thread1: finishing: ", cast[pointer](myBytes).repr
-    
-    GC_fullCollect()
+    var myBytes: ref Buffer
+    myBytes.new:
+      proc (x: ref Buffer) =
+        echo "thread1: freeing: ", cast[pointer](x).repr
+    # myBytes.new()
+    myBytes.data = 10
+    shareSeq = myBytes
+    echo "thread1: sent, left over: ", repr myBytes
     signal(event)
+    wait(event)
+    # myBytes = nil
+    echo "thread1: finishing: ", cast[pointer](myBytes).repr
+
+    GC_fullCollect()
+    signal(eventAfterGcFree)
     os.sleep(100)
 
 proc thread2(val: int) {.thread.} =
@@ -50,14 +50,10 @@ proc thread2(val: int) {.thread.} =
     var msg = shareSeq
     shareSeq = nil
     echo "thread2: shared moved: ", cast[pointer](shareSeq).repr
-    # GC_ref(msg)
     if msg != nil:
       echo "thread2: received: ", repr msg
-    # echo "thread2: deref: "
-    # GC_unref(msg)
-    # msg = nil
     signal(event)
-    wait(event)
+    wait(eventAfterGcFree)
     echo "thread2: wait check: ", repr msg
     echo "thread2: done: "
 
@@ -65,6 +61,7 @@ proc main() =
   echo "running"
 
   event = initEvent()
+  eventAfterGcFree = initEvent()
   createThread(threads[0], thread1, 0)
   createThread(threads[1], thread2, 1)
 
